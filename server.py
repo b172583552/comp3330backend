dbba0188ce778f2d0f05aff60e4ca6e58dad20e2
tom <b172583552@gmail.com>
2023-11-16 04:28:30 +0800

    update post function

--- server.py
+++ server.py
@@ -2,24 +2,64 @@ import json
 import sqlite3
 
 import flask
+from flask import request, jsonify
+
 app = flask.Flask(__name__)
-#app.config["DEBUG"] = True # Enable debug mode to enable hot-reloader.
-@app.route('/jobs')
+
+
+# app.config["DEBUG"] = True # Enable debug mode to enable hot-reloader.
+@app.route('/jobs', methods=["GET", "POST"])
 def job():
-    con = sqlite3.connect('caretaker.db')
-    cursor = con.execute("SELECT * from JOBS;")
-    allJobs = []
-    for row in cursor:
-        jobs = {}
-        jobs["id"] = row[0]
-        jobs["userid"] = row[1]
-        jobs["jobtitle"] = row[2]
-        jobs["place"] = row[3]
-        jobs["jobdetails"] = row[4]
-        jobs["jobtime"] = row[5]
-        allJobs.append(jobs)
-    con.close()
-    outdata = json.dumps(allJobs)
-    return outdata
+
+
+    if request.method == "POST":
+        con = sqlite3.connect('caretaker.db')
+        print(request.form.values())
+        userId = request.form.get('userId')
+        print(userId)
+        careTakingID = request.form.get('careTakingId')
+        print(userId)
+        cursor = con.execute("SELECT * FROM JOBS where UserID = " + userId +" and ID = " + careTakingID)
+        row = cursor.fetchone()
+        print(row)
+
+        if row is None:
+            sql = "UPDATE JOBS SET UserID = " + userId + " where ID = " + careTakingID +";"
+            print(sql)
+            cursor = con.execute(sql)
+            con.commit()
+            con.close()
+            return jsonify(success=True), 200
+        else:
+            sql = "UPDATE JOBS SET UserID = NULL where ID = "+careTakingID+";"
+            print(sql)
+            cursor = con.execute(sql)
+            con.commit()
+            con.close()
+            return jsonify(success=True), 200
+
+
+    if request.method == "GET":
+        con = sqlite3.connect('caretaker.db')
+        cursor = con.execute("SELECT * from JOBS;")
+        allJobs = []
+        for row in cursor:
+            jobs = {}
+            jobs["id"] = row[0]
+            jobs["userId"] = row[1]
+            jobs["jobTitle"] = row[2]
+            jobs["place"] = row[3]
+            jobs["jobDetails"] = row[4]
+            jobs["jobTime"] = row[5]
+            jobs["salary"] = row[6]
+            allJobs.append(jobs)
+        outdata = json.dumps(allJobs)
+        con.close()
+        return outdata
+
+
+
+
+
 # adds host="0.0.0.0" to make the server publicly available
 app.run(host="0.0.0.0")
