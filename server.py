1d35a1d2a97c992bc4b55dcf0d0fb48363f0c56f
soniazzz <soniaz05@connect.hku.hk>
2023-11-16 21:52:27 +0800

    set up database for patient and health statistics

--- server.py
+++ server.py
@@ -2,6 +2,11 @@ import json
 import sqlite3
 
 import flask
+from flask import jsonify, request
+
+
+
+
 app = flask.Flask(__name__)
 #app.config["DEBUG"] = True # Enable debug mode to enable hot-reloader.
 @app.route('/jobs')
@@ -21,5 +26,107 @@ def job():
     con.close()
     outdata = json.dumps(allJobs)
     return outdata
+
+
+@app.route('/patients', methods=['GET'])
+def get_patients():
+    carer_id = request.args.get('carer_id', default=1, type=int)
+    conn = sqlite3.connect('caretaker.db')
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM Patients WHERE carer_id=?", (carer_id,))
+    patients = cur.fetchall()
+    conn.close()
+
+
+    patients_list = []
+    for patient in patients:
+        patients_list.append(
+            {
+                "id": patient[0],
+                "carer_id": patient[1],
+                "name": patient[2],
+                "sex": patient[3],
+                "age": patient[4],
+                "address": patient[5],
+                "tel": patient[6],
+                "emergency_contact": patient[7],
+                "emergency_number": patient[8]
+            }
+        )
+
+    return jsonify(patients_list)
+
+@app.route('/health_statistics', methods=['GET'])
+def get_health_statistics():
+    carer_id = request.args.get('carer_id', default=1, type=int)
+    conn = sqlite3.connect('caretaker.db')
+    cur = conn.cursor()
+
+    cur.execute('''SELECT * FROM HealthStatistics,Patients 
+                    WHERE HealthStatistics.patient_id=Patients.patient_id AND carer_id=?''', (carer_id,))
+    statistics = cur.fetchall()
+
+    conn.close()
+    health_statistics = []
+    for stat in statistics:
+        patient = {
+            "id": str(stat[0]),
+            "name": stat[9],
+            "gender": stat[10],
+            "age": str(stat[11]),
+            "health_statistics": {
+                "height": str(stat[1]),
+                "weight": str(stat[2]),
+                "blood_pressure": stat[3],
+                "pulse_rate": str(stat[4]),
+                "medical_history": json.loads(stat[5]),  # assuming medical_history is stored in json format
+                "blood_oxygen": str(stat[6])
+            }
+        }
+        health_statistics.append(patient)
+
+    return jsonify(health_statistics)
+
+
+@app.route('/update_health_statistics', methods=['GET'])
+def update_health_statistics():
+    patient_id = request.args.get('patient_id', default=1, type=int)
+    conn = sqlite3.connect('caretaker.db')
+    cur = conn.cursor()
+
+    cur.execute('''SELECT * FROM HealthStatistics,Patients 
+                        WHERE HealthStatistics.patient_id=Patients.patient_id AND Patients.patient_id=?''', (patient_id,))
+    stat = cur.fetchall()[0]
+
+
+    conn.close()
+    import random
+    delta1=random.randint(-4, 4)
+    delta2 = random.randint(-3, 3)
+    delta3 = random.randint(-3, 3)
+    delta4 = random.randint(-3, 3)
+    delta5 = random.randint(-5, 5)
+    b = stat[3].split('/')
+    b[0] = int(b[0]) + delta2
+    b[1] = int(b[1]) + delta3
+    b = str(b[0]) + '/' + str(b[1])
+    patient = {
+            "id": str(stat[0]),
+            "name": stat[9],
+            "gender": stat[10],
+            "age": str(stat[11]),
+            "health_statistics": {
+                "height": str(stat[1]),
+                "weight": str(stat[2]+delta1),
+                "blood_pressure": b,
+                "pulse_rate": str(stat[4]+delta4),
+                "medical_history": json.loads(stat[5]),  # assuming medical_history is stored in json format
+                "blood_oxygen": str(stat[6]+delta5)
+            }
+    }
+
+
+    return jsonify(patient)
 # adds host="0.0.0.0" to make the server publicly available
 app.run(host="0.0.0.0")
